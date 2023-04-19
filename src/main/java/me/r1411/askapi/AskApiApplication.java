4a8e1208060cea5fc1e5f6c3c17442abb4da1ebd
r1411 <roma00055@gmail.com>
2023-04-19 22:36:11 +0300

    Initial commit

---
+++ src/main/java/me/r1411/askapi/AskApiApplication.java
@@ -0,0 +1,13 @@
+package me.r1411.askapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AskApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AskApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/r1411/askapi/AskApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.r1411.askapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AskApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
