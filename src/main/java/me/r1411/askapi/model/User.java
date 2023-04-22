780bbc0679dc22648d969a70fb7d80699eb992ca
r1411 <roma00055@gmail.com>
2023-04-22 16:04:45 +0300

    Модель User

---
+++ src/main/java/me/r1411/askapi/model/User.java
@@ -0,0 +1,50 @@
+package me.r1411.askapi.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Size(min = 4, max = 32, message = "Username length should be between 4 and 32 characters long")
+    @Pattern(regexp = "^[a-zA-Z0-9_]+$", message = "Username can only contain numbers, letters and underscores")
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    public User() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
