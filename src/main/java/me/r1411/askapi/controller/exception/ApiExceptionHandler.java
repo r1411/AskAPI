42801b83a68821d04f1ca3503d51962829132da0
r1411 <roma00055@gmail.com>
2023-05-05 22:48:30 +0300

    Обработка ошибки отсутствия прав у пользователя

--- src/main/java/me/r1411/askapi/controller/exception/ApiExceptionHandler.java
+++ src/main/java/me/r1411/askapi/controller/exception/ApiExceptionHandler.java
@@ -4,6 +4,7 @@ import me.r1411.askapi.controller.wrapper.ErrorResponseEntity;
 import me.r1411.askapi.dto.error.ErrorResponseDto;
 import me.r1411.askapi.dto.error.ValidationErrorResponseDto;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -24,6 +25,11 @@ public class ApiExceptionHandler {
     @ExceptionHandler
     public ErrorResponseEntity<ValidationErrorResponseDto> validationException(MethodArgumentNotValidException e) {
         return new ErrorResponseEntity<>(new ValidationErrorResponseDto("Validation failed", getValidationErrors(e.getBindingResult())), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    public ErrorResponseEntity<ErrorResponseDto> accessDeniedException(AccessDeniedException e) {
+        return new ErrorResponseEntity<>(new ErrorResponseDto("You don't have permission to perform this action"), HttpStatus.FORBIDDEN);
     }
 
     @ExceptionHandler(Exception.class)
